9c0702ac00ca7a9e38f7c2c13b391276fd02575d
Adrian Arpi <arpiadrian5@gmail.com>
2019-06-18 18:16:04 +0200

    Comentario de tipo JavaDoc

---
+++ src/problemas/Venta.java
@@ -0,0 +1,14 @@
+package problemas;
+
+public class Venta {
+
+	/**
+	 * Nueva prueba....
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
